f3e0976f35fb66eea3578b43493007e542fe6a2c
Amanda Dadalt Makino <adadaltmakino@academic.rrc.ca>
2025-09-12 13:20:06 -0500

    Defined update balance, deposit, and withdraw methods. Also defined the str method.

--- bank_account/bank_account.py
+++ bank_account/bank_account.py
@@ -44,7 +44,7 @@ class BankAccount:
         else:
             try:
                 self.__balance = float(balance)
-            except (ValueError, TypeError):
+            except (ValueError):
                 self.__balance = 0.0
 
     @property
@@ -80,3 +80,85 @@ class BankAccount:
         """
         return self.__balance
 
+    def update_balance(self, amount: float) -> None:
+        """
+        Updates the balance according to the amount.
+
+        Args:
+            amount(float): The value that is going be applied to
+            the balance.
+        """
+
+        valid = True
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            valid = False
+
+        if valid:
+            self.__balance += amount
+
+
+    def deposit(self, amount: float) -> None:
+        """
+        Simulates the deposit according to the amount value.
+
+        Args:
+            amount(float): The value that is going be added to the 
+            balance.
+        
+        Raises:
+            ValueError: If amount is not numeric, and if amount is not
+            positive.
+        """
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ValueError(f"Deposit amount: {amount} must be numeric.")
+
+        if amount <= 0:
+            raise ValueError(f"Deposit amount: ${amount:.2f} must be positive.")
+
+        self.update_balance(amount)
+
+
+    def withdraw(self, amount: float) -> None:
+        """
+        Simulates the withdrawal process according to the amount value.
+
+        Args:
+            amount(float): The value that is going be subtracted off 
+            the balance.
+        
+        Raises:
+            ValueError: if amount is not numeric, and if amount is
+            negative, and if amount exceeds the current balance.
+        """
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ValueError(f"Withdraw amount: {amount} must be numeric.")
+
+        if amount <= 0:
+            raise ValueError(f"Withdraw amount: ${amount:.2f} must be positive.")
+
+        if amount > self.__balance:
+            raise ValueError(
+                f"Withdraw amount: ${amount:.2f} must not exceed the account balance: ${self.__balance:.2f}"
+            )
+
+        self.update_balance(-amount)
+
+
+    def __str__(self):
+        """
+        Returns a string representation of the class instance.
+
+        Returns:
+            str: The course instance formatted as a string.
+        """
+
+        return f"Account Number: {self.__account_number} Balance: ${self.__balance:.2f}"
