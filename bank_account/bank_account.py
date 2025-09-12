269602c895a03396392d4f83122cf8a770d07931
Amanda Dadalt Makino <adadaltmakino@academic.rrc.ca>
2025-09-12 12:02:55 -0500

    Implemented the init according to the requirements.

--- bank_account/bank_account.py
+++ bank_account/bank_account.py
@@ -21,5 +21,28 @@ class BankAccount:
             client number representing the account holder.
             balance(float): A float value representing the current 
             balance of the bank account.
+        
+        Raises:
+            ValueError: If account number is not an integer, if client
+            number is not an integer, and if balance is not a float
+            and cannot be transferred to a float.
 
         """
+
+        if isinstance(account_number, int):
+            self.__account_number = account_number
+        else:
+            raise ValueError("Account number must be numeric.")
+
+        if isinstance(client_number, int):
+            self.__client_number = client_number
+        else:
+            raise ValueError("Client number must be numeric.")
+
+        if isinstance (balance, float):
+            self.__balance = balance
+        else:
+            try:
+                self.__balance = float(balance)
+            except (ValueError, TypeError):
+                self.__balance = 0.0
