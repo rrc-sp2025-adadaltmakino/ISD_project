e9130859dbf19681cd8cd3c531f9b21ec8f34a9c
Amanda Dadalt Makino <adadaltmakino@academic.rrc.ca>
2025-09-29 14:47:56 -0500

    Started setting up instance of the overdraft limit.

--- bank_account/chequing_account.py
+++ bank_account/chequing_account.py
@@ -13,27 +13,34 @@ class ChequingAccount(BankAccount):
     """
 
     def __init__(self, overdraft_limit:float, overdraft_rate:float,
-                 account_number:int, client_number:int, balance:float,
-                 date_created:date):
+                 BASE_SERVICE_CHARGE: float, account_number:int, 
+                 client_number:int, balance:float, date_created:date):
         """
         Initializes class attributes to argument values.
 
         Args:
-            overdraft_limit(float): The maximum amount a balance can 
+            - overdraft_limit(float): The maximum amount a balance can 
             be overdrawn (below 0.00) before overdraft fees are 
             applied.
-            overdraft_rate(float): The rate to which overdraft fees 
+            - overdraft_rate(float): The rate to which overdraft fees 
             will be applied.
-            account_number(int):  An integer value representing the 
+            - account_number(int):  An integer value representing the 
             bank account number.
-            client_number(int):  An integer value representing the 
+            - client_number(int):  An integer value representing the 
             client number representing the account holder.
-            balance(float): A float value representing the current 
+            - balance(float): A float value representing the current 
             balance of the bank account.
-            date_created(date): Date representing the date created.
+            # date_created(date): Date representing the date created.
         """
 
-        super().__init__(account_number, client_number, balance, date_created)
+        super().__init__(BASE_SERVICE_CHARGE, account_number, client_number,
+                         balance, date_created)
+
+        if isinstance(overdraft_limit, float):
+            self.__overdraft_limit = overdraft_limit
+        else:
+            raise ValueError ("")
+
 
 
     def __str__(self) -> str:
