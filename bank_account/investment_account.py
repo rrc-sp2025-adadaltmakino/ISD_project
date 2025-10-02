fa6d746d4388b90db5e068a29e98fd48f1ced81d
Amanda Dadalt Makino <adadaltmakino@academic.rrc.ca>
2025-10-02 05:02:42 -0500

    Implemented __str__ method

--- bank_account/investment_account.py
+++ bank_account/investment_account.py
@@ -33,4 +33,35 @@ class InvestmentAccount(BankAccount):
         """
         super().__init__(account_number, client_number, balance, date_created)
 
-        self.__management_fee = management_fee
+        try:
+            self.__management_fee = float(management_fee)
+        except ValueError:
+            self.__management_fee = 2.55
+
+
+    def __str__(self):
+        """
+        Returns a string representation of the InvestmentAccount class
+        instance.
+
+        Returns:
+            str: The Investment Account instance formatted as a string.
+        """
+        bank_account_string = super().__str__()
+
+        if self._date_created <= InvestmentAccount.TEN_YEARS_AGO:
+            management_fee_string = "Waived"
+        else:
+            management_fee_string = f"${self.__management_fee:,.2f}"
+
+        investment_account_string = (f"Date Created: {self._date_created} "
+                                + f"Management Fee: ${management_fee_string} "
+                                + "Account Type: Investment")
+
+        return (bank_account_string + investment_account_string)
+
+
+    def get_service_charges(self):
+        """
+        
+        """
