0bd4f5abe705c50480ab14beb4192f95e5d44301
Amanda Dadalt Makino <adadaltmakino@academic.rrc.ca>
2025-10-22 19:50:10 -0500

    Defined the OverdraftStrategy class and its method

--- patterns/strategy/overdraft_strategy.py
+++ patterns/strategy/overdraft_strategy.py
@@ -4,10 +4,10 @@ Description: A class to manage Overdraft Strategy class  objects.
 __author__ = "Amanda Dadalt Makino"
 __version__ = "1.0.0"
 
-from abc import ABC, abstractmethod
+from patterns.strategy.service_charge_strategy import ServiceChargeStrategy
 from bank_account.bank_account import BankAccount
 
-class OverdraftStrategy:
+class OverdraftStrategy(ServiceChargeStrategy):
     """
     OverdraftStrategy class: Maintains overdraft strategy data.
     """
@@ -21,27 +21,28 @@ class OverdraftStrategy:
             overdraft limit.
             - overdraft_rate(float): Value that represents the overdraft
             rate.
-
-        Raises:
-
         """
-        self._overdraft_limit = overdraft_limit
-        self._overdraft_rate = overdraft_rate
+        self.__overdraft_limit = overdraft_limit
+        self.__overdraft_rate = overdraft_rate
 
 
-    def calculate_service_charges(self) -> float:
+    def calculate_service_charges(self, account: BankAccount) -> float:
         """
         Simulates the calculation of service charges according to the 
         overdraft limit, and overdraft rate.
 
+        Args:
+            account(BankAccount): The bank account that will be used.
+
         Returns:
             float: The calculated service rate.
         """
+        balance = account.get_balance()
 
-        # if self.balance >= self.__overdraft_limit:
-        #     return super().BASE_SERVICE_CHARGE
-
-        # else:
-        #     return (super().BASE_SERVICE_CHARGE +
-        #             (self.__overdraft_limit - self.balance)
-        #             * self.__overdraft_rate)
+        if balance >= self.__overdraft_limit:
+            return self.BASE_SERVICE_CHARGE
+        else:
+            return (
+                self.BASE_SERVICE_CHARGE + (self.__overdraft_limit - balance)
+                * self.__overdraft_rate
+            )
