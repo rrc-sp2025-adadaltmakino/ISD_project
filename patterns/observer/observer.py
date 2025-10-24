ab2887336fb4a7bed246f6109f844aa898135665
Amanda Dadalt Makino <adadaltmakino@academic.rrc.ca>
2025-10-24 08:10:33 -0500

    Observer class created

---
+++ patterns/observer/observer.py
@@ -0,0 +1,20 @@
+""""
+Description: A class to manage Observer class objects.
+"""
+__author__ = "Amanda Dadalt Makino"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+
+class Oberver(ABC):
+    """
+    Observer class: Maintains observer data.
+    """
+
+    @abstractmethod
+    def update(self, message: str) -> None:
+        """
+        Abstract method
+        Implemented in subclass(es).
+        """
+        pass
