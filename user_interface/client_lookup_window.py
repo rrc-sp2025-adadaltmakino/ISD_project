603fde311f922ce060736bd91cee195eaf256fb4
Amanda Dadalt Makino <adadaltmakino@academic.rrc.ca>
2025-11-13 15:05:51 -0600

    Established __init__() and the connections between widgets in the ClientLookupWindow class

--- user_interface/client_lookup_window.py
+++ user_interface/client_lookup_window.py
@@ -1,6 +1,6 @@
 __author__ = "ACE Faculty"
 __version__ = "1.0.0"
-__credits__ = ""
+__credits__ = "Amanda Dadalt Makino"
 
 from PySide6.QtWidgets import QTableWidgetItem, QMessageBox
 from PySide6.QtCore import Qt
@@ -12,5 +12,19 @@ from user_interface.manage_data import update_data
 from bank_account.bank_account import BankAccount
 
 class ClientLookupWindow(LookupWindow):
-    pass
-        
+    
+    def __init__(self):
+        """
+        Initializes class attributes to argument values.
+        """
+        super().__init__()
+
+        # call load_data() and set attribute to the value
+        client_listing, accounts = load_data()
+        self.client_listing = client_listing
+        self.accounts = accounts
+
+        # establishing connnections
+        self.lookup_button.clicked.connect(self.on_lookup_client) #lookup_button - clicked - lookup_client()
+        self.client_number_edit.textChanged.connect(self.on_text_changed) #client_number_edit - textChanged - on_text_changed()
+        self.account_table.cellClicked.connect(self.on_select_account) #account_table - cellClicked - on_select_account()
